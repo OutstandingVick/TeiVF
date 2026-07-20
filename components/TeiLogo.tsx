98b667a346b9b75cb83bb9347fecb3ef9926c011
OutstandingVick <scottdodo2015@outlook.com>
2026-07-20 13:26:33 +0100

    fuzzy logo fixed

--- components/TeiLogo.tsx
+++ components/TeiLogo.tsx
@@ -1,12 +1,3 @@
 export function TeiLogo() {
-  return (
-    <svg className="tei-logo" viewBox="0 0 128 82" role="img" aria-hidden="true">
-      <polygon className="logo-green" points="24 12 84 12 76 30 59 30 37 70 8 70 33 30 16 30" />
-      <polygon className="logo-white" points="82 12 117 12 109 30 96 30 76 70 43 70 51 52 64 52 75 30" />
-      <polygon className="logo-obsidian" points="71 30 86 30 78 48 63 48" />
-      <polygon className="logo-obsidian" points="58 52 75 52 67 70 50 70" />
-      <polygon className="logo-white" points="86 34 112 34 105 49 79 49" />
-      <polygon className="logo-white" points="75 55 102 55 95 70 68 70" />
-    </svg>
-  );
+  return <img className="tei-logo" src="/tei-velocity-mark.png" alt="Tei" width="557" height="437" />;
 }
